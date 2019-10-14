282d255ea20f7cd81d6a818e491528184998426b
paolofrs <p.fresch@outlook.it>
2019-10-15 00:37:40 +0200

    added firt assembly source code, not assembled yet

---
+++ raspbi3/introduction/first.s
@@ -0,0 +1,16 @@
+/* this is my first assembly source */
+
+
+	; this is a comment 
+	
+.global main 			; main has .global directive to be seen by all other source modules (source code files)   
+
+main:	 			; this is the main label (i.e. the istruction loaded into pc when main is executed)
+	
+/* NB
+the GNU linker specifically requires a _start symbol to specify the first instruction to be executed in a program
+*/
+
+	mov r0, #2 		; put 2 inside r0
+	bx lr			; return from main
+	
